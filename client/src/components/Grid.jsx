c111153b41a68774d356b81e0f71c242cb19f02c
Alex Bazlinton <alex.bazlinton@codeclan.com>
2017-06-08 09:19:50 +0100

    formatting

--- client/src/components/Grid.jsx
+++ client/src/components/Grid.jsx
@@ -4,13 +4,18 @@ import Cell from './Cell';
 const Grid = ({grid, takeCell}) => {
 
   const cells = grid.map(function(cell, index){
-    return <Cell key={index} player={cell} location={index} takeCell={takeCell}/>
+    return (
+      <Cell 
+        key={index} 
+        player={cell} 
+        location={index} 
+        takeCell={takeCell}
+      />)
   })
-
 
   return(
     <div id="grid">
-    {cells}
+      {cells}
     </div>
   )
 
