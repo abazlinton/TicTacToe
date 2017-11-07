00b9113f94f1162b8ee63500df64f9625274892e
Alex Bazlinton <abazlinton@gmail.com>
2017-11-07 18:25:31 +0000

    make master master

--- client/src/app.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import TicTacToe from './containers/TicTacToe';
-
-window.onload = function(){
-  const app = document.getElementById('app');
-  ReactDOM.render(<TicTacToe/>, app);
-};
