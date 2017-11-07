893f889d8c9a8c9079078e17602696747e2dd1ee
Alex Bazlinton <abazlinton@gmail.com>
2017-11-07 19:01:41 +0000

    remove assert?

--- server.js
+++ server.js
@@ -7,9 +7,7 @@ app.use(express.static(__dirname + '/client/build'));
 
 app.listen(process.env.PORT, () => {
   MongoClient.connect(process.env.MONGODB_URI, function(err, db) {
-    assert.equal(null, err);
     console.log("Connected successfully to server");
-  
     db.close();
   });
 });
