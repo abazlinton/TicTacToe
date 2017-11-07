bcea9b548308cc56c26960980255650d7abcf78e
Alex Bazlinton <abazlinton@gmail.com>
2017-11-07 19:05:11 +0000

    test insert

--- server.js
+++ server.js
@@ -7,7 +7,16 @@ app.use(express.static(__dirname + '/client/build'));
 
 app.listen(process.env.PORT, () => {
   MongoClient.connect(process.env.MONGODB_URI, function(err, db) {
-    console.log("Connected successfully to server");
+   
+      var collection = db.collection('documents');
+      // Insert some documents
+      collection.insertMany([
+        {a : 1}, {a : 2}, {a : 3}
+      ], function(err, result) {
+        console.log("Inserted 3 documents into the collection");
+        console.log(result);
+      });
+    }
     db.close();
   });
 });
